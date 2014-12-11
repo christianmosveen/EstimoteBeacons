287c0e058bd26ee9801333d2747036bf3b553879
Christian H. Mosveen <neypalm@gmail.com>
2014-12-11 16:22:09 +0100

    Trakk ut konstantene i en egen klasse

---
+++ EstimoteBeacons/Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class Constants {
+    let beaconUUID:NSUUID = NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
+    let MAJOR = 1337 // SETT DENNE TIL EN UNIK MAJOR-ID
+    
+    let colors = [
+        1: UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1),
+        2: UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1),
+        3: UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+    ]
+}
--- EstimoteBeacons/ViewController.swift
+++ EstimoteBeacons/ViewController.swift
@@ -2,16 +2,8 @@ import UIKit
 
 class ViewController: UIViewController, ESTBeaconManagerDelegate {
 
+    let constants:Constants = Constants()
     let beaconManager:ESTBeaconManager = ESTBeaconManager()
-    
-    let beaconUUID:NSUUID = NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
-    let MAJOR = 1337 // SETT DENNE TIL EGEN MAJOR-ID
-    
-    let colors = [
-        1: UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1),
-        2: UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1),
-        3: UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +11,7 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         beaconManager.requestAlwaysAuthorization()
         beaconManager.delegate = self
         
-        var beaconRegion:ESTBeaconRegion = ESTBeaconRegion(proximityUUID: beaconUUID, identifier: "Estimote")
+        var beaconRegion:ESTBeaconRegion = ESTBeaconRegion(proximityUUID: constants.beaconUUID, identifier: "Estimote")
         
         beaconManager.startRangingBeaconsInRegion(beaconRegion)
     }
@@ -28,11 +20,11 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         
         println("Found \(beacons.count) beacons!")
         
-        let myBeacons = beacons.filter{$0.major.integerValue == self.MAJOR}
+        let myBeacons = beacons.filter{$0.major.integerValue == self.constants.MAJOR}
         
         if (myBeacons.count > 0) {
             let proximity = myBeacons[0].proximity.rawValue;
-            self.view.backgroundColor = self.colors[proximity];
+            self.view.backgroundColor = constants.colors[proximity];
         }
     }
 }
